71defec1f7e240e25b4c9aede0e36c242139612b
Póka Sándor Zsolt <ZephyriusX1999@Gmail.Com>
2016-10-08 16:41:54 +0200

    Update Main.php

--- src/ZephyriusX/ServerInfoPE/Main.php
+++ src/ZephyriusX/ServerInfoPE/Main.php
@@ -2,9 +2,9 @@
 
 namespace ZephyriusX\ServerInfoPE;
 
-use
-use
-use
-use
-use
-use
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExcutor;
+use pocketmine\
+use pocketmine\
